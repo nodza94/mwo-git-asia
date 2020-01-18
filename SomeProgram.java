c8db92f416f79453c205a5922400cbe620e395f3
Joanna Nodzynska <joanna.nodzynska@gmail.com>
2020-01-18 09:52:31 +0100

    Added committment line to the code

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,9 @@
 public class SomeProgram { 
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
+      System.oput.println("We will be Git experts, soon");
     }
 }
+
+
+
